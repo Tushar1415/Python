f5ccf0e6d15f768b93ad3e724f72cf5d9a36524a
Tushar Mohite <admin@mohitetax.in>
2019-11-14 15:16:28 +0530

    added captcha training app

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -3,8 +3,8 @@ from selenium.webdriver.chrome.options import Options
 import urllib
 from selenium.webdriver.common.keys import Keys
 import time
-#user_name = "100156819901"
-#password = "Champ@2018"
+user_name = "100156819901"
+password = "Champ@2018"
 #options = Options()
 #options.headless = True
 #driver = webdriver.Chrome(executable_path='chromedriver', chrome_options=options)
@@ -13,34 +13,36 @@ driver = webdriver.Chrome()
 driver.maximize_window()
 driver.get("https://unifiedportal-mem.epfindia.gov.in/memberinterface/")
 
-string_path = "D:\\Captcha2\\Captcha"
-i = 156
-while i < 1001:
-	if (i > 9999 and i < 100000): 
-		string_path1 = string_path + "0" + str(i) + ".png"
-	if (i > 999 and i < 10000): 
-		string_path1 = string_path + "00" + str(i) + ".png"
-	if (i > 99 and i < 1000): 
-			string_path1 = string_path + "000" + str(i) + ".png"
-	if (i > 9 and i < 100): 
-		string_path1 = string_path + "0000" + str(i) + ".png"
-	if i < 9:
-		string_path1 = string_path + "00000" + str(i) + ".png"
-	with open(string_path1, 'wb') as file:
-		file.write(driver.find_element_by_xpath("/html/body/div[2]/div/div[3]/div[1]/div[2]/div/div[2]/div/form/div[4]/div[1]/div/img").screenshot_as_png)
-		driver.refresh();
-		i += 1
-driver.close()
+#string_path = "D:\\Captcha3\\Captcha"
+#i = 1
+#while i < 100001:
+#	if (i > 99999): 
+#		string_path1 = string_path + str(i) + ".png"
+#	if (i > 9999 and i < 100000): 
+#		string_path1 = string_path + "0" + str(i) + ".png"
+#	if (i > 999 and i < 10000): 
+#		string_path1 = string_path + "00" + str(i) + ".png"
+#	if (i > 99 and i < 1000): 
+#			string_path1 = string_path + "000" + str(i) + ".png"
+#	if (i > 9 and i < 100): 
+#		string_path1 = string_path + "0000" + str(i) + ".png"
+#	if i < 9:
+#		string_path1 = string_path + "00000" + str(i) + ".png"
+#	with open(string_path1, 'wb') as file:
+#		file.write(driver.find_element_by_xpath("/html/body/div[2]/div/div[3]/div[1]/div[2]/div/div[2]/div/form/div[4]/div[1]/div/img").screenshot_as_png)
+#		driver.refresh();
+#		i += 1
+#driver.close()
 
-#element = driver.find_element_by_id("userName")
-#element.send_keys(user_name)
-#element = driver.find_element_by_id("password")
-#element.send_keys(password)
-#time.sleep(10)
-#element = driver.find_element_by_xpath("/html/body/div[2]/div/div[3]/div[1]/div[2]/div/div[2]/div/form/div[5]/div[2]/button")
-#element.click()
-#time.sleep(1)
-#element = driver.find_element_by_id("btnCloseModal")
-#element.click()
-#time.sleep(3)
-#element.close()
+element = driver.find_element_by_id("userName")
+element.send_keys(user_name)
+element = driver.find_element_by_id("password")
+element.send_keys(password)
+time.sleep(10)
+element = driver.find_element_by_xpath("/html/body/div[2]/div/div[3]/div[1]/div[2]/div/div[2]/div/form/div[5]/div[2]/button")
+element.click()
+time.sleep(1)
+element = driver.find_element_by_id("btnCloseModal")
+element.click()
+time.sleep(3)
+element.close()
